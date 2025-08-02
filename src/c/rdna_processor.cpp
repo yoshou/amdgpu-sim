c5cf41865cffd30f9e44b304dd8bf10f7a16fc07
yoshou <yoshou.dev@gmail.com>
2025-08-02 16:30:31 +0900

    Fix warnings

--- src/c/rdna_processor.cpp
+++ src/c/rdna_processor.cpp
@@ -14,8 +14,18 @@ void v_mul_f64_e32(uint16_t d, uint16_t s0, uint16_t s1, const int32_t *sgprs, i
                    const int32_t *literal_constant);
 }
 
+struct m256dx2 {
+  __m256d value0;
+  __m256d value1;
+
+  m256dx2(__m256d v0, __m256d v1) : value0(v0), value1(v1) {}
+  m256dx2() : value0(_mm256_setzero_pd()), value1(_mm256_setzero_pd()) {}
+};
+
 template <size_t idx>
-static std::tuple<__m256d, __m256d> abs_neg(__m256d value0, __m256d value1, uint8_t abs, uint8_t neg) {
+static m256dx2 abs_neg(const m256dx2 value, uint8_t abs, uint8_t neg) {
+  auto value0 = value.value0;
+  auto value1 = value.value1;
   if ((abs >> idx) & 1) {
     value0 = _mm256_andnot_pd(value0, _mm256_set1_pd(-0.0));
     value1 = _mm256_andnot_pd(value1, _mm256_set1_pd(-0.0));
@@ -24,7 +34,7 @@ static std::tuple<__m256d, __m256d> abs_neg(__m256d value0, __m256d value1, uint
     value0 = _mm256_xor_pd(value0, _mm256_set1_pd(-0.0));
     value1 = _mm256_xor_pd(value1, _mm256_set1_pd(-0.0));
   }
-  return std::make_tuple(value0, value1);
+  return m256dx2(value0, value1);
 }
 
 static uint64_t get_scalar_operand_u64_e64(int addr, const int32_t *sgprs) {
@@ -61,16 +71,22 @@ static uint64_t get_scalar_operand_u64_e64(int addr, const int32_t *sgprs) {
 }
 
 static double get_scalar_operand_e64_f64(int addr, const int32_t *sgprs, const int32_t *literal_constant) {
+  union {
+    uint64_t u64;
+    double f64;
+  } value;
+
   if (addr == 255) {
-    const auto value = static_cast<uint64_t>(literal_constant[0]) << 32;
-    return *reinterpret_cast<const double *>(&value);
+    value.u64 = static_cast<uint64_t>(literal_constant[0]) << 32;
   } else {
-    const auto value = get_scalar_operand_u64_e64(addr, sgprs);
-    return *reinterpret_cast<const double *>(&value);
+    value.u64 = get_scalar_operand_u64_e64(addr, sgprs);
   }
+  return value.f64;
 }
 
-static std::tuple<__m256d, __m256d> omod_clamp(__m256d value0, __m256d value1, uint8_t omod, bool clamp) {
+static m256dx2 omod_clamp(const m256dx2 value, uint8_t omod, bool clamp) {
+  auto value0 = value.value0;
+  auto value1 = value.value1;
   if (omod == 1) {
     value0 = _mm256_mul_pd(value0, _mm256_set1_pd(2.0));
     value1 = _mm256_mul_pd(value1, _mm256_set1_pd(2.0));
@@ -89,18 +105,20 @@ static std::tuple<__m256d, __m256d> omod_clamp(__m256d value0, __m256d value1, u
     value1 = _mm256_max_pd(zero, _mm256_min_pd(value1, one));
   }
 
-  return std::make_tuple(value0, value1);
+  return m256dx2(value0, value1);
 }
 
-static std::tuple<__m256d, __m256d> load_vgpr(int elem, int idx, const int32_t *vgprs) {
+static m256dx2 load_vgpr(int elem, int idx, const int32_t *vgprs) {
   const auto value_lo = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(&vgprs[(idx * 32 + elem)]));
   const auto value_hi = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(&vgprs[(idx * 32 + elem + 32)]));
   const auto value0 = _mm256_castsi256_pd(_mm256_unpacklo_epi32(value_lo, value_hi));
   const auto value1 = _mm256_castsi256_pd(_mm256_unpackhi_epi32(value_lo, value_hi));
-  return std::make_tuple(value0, value1);
+  return m256dx2(value0, value1);
 }
 
-static void store_vgpr(int elem, int idx, __m256d value0, __m256d value1, int32_t *vgprs, __m256i mask) {
+static void store_vgpr(int elem, int idx, const m256dx2 value, int32_t *vgprs, __m256i mask) {
+  const auto value0 = value.value0;
+  const auto value1 = value.value1;
   const auto value0_ps = _mm256_castpd_ps(value0);
   const auto value1_ps = _mm256_castpd_ps(value1);
   const auto value_lo = _mm256_castps_si256(_mm256_shuffle_ps(value0_ps, value1_ps, 0x88));
@@ -118,42 +136,39 @@ void v_fma_f64(uint16_t d, uint16_t s0, uint16_t s1, uint16_t s2, uint8_t abs, u
     const auto i = static_cast<int32_t>(sgprs[126] >> elem);
     const auto mask = _mm256_cmpeq_epi32(_mm256_and_si256(_mm256_set1_epi32(i), bitpos), bitpos);
 
-    __m256d s0_value0, s0_value1, s1_value0, s1_value1, s2_value0, s2_value1;
+    m256dx2 s0_value, s1_value, s2_value;
 
     if (s0 >= 256) {
-      std::tie(s0_value0, s0_value1) = load_vgpr(elem, s0 - 256, vgprs);
+      s0_value = load_vgpr(elem, s0 - 256, vgprs);
     } else {
       const auto value = get_scalar_operand_e64_f64(s0, sgprs, literal_constant);
-      s0_value0 = _mm256_set1_pd(value);
-      s0_value1 = _mm256_set1_pd(value);
+      s0_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
     if (s1 >= 256) {
-      std::tie(s1_value0, s1_value1) = load_vgpr(elem, s1 - 256, vgprs);
+      s1_value = load_vgpr(elem, s1 - 256, vgprs);
     } else {
       const auto value = get_scalar_operand_e64_f64(s1, sgprs, literal_constant);
-      s1_value0 = _mm256_set1_pd(value);
-      s1_value1 = _mm256_set1_pd(value);
+      s1_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
     if (s2 >= 256) {
-      std::tie(s2_value0, s2_value1) = load_vgpr(elem, s2 - 256, vgprs);
+      s2_value = load_vgpr(elem, s2 - 256, vgprs);
     } else {
       double value = get_scalar_operand_e64_f64(s2, sgprs, literal_constant);
-      s2_value0 = _mm256_set1_pd(value);
-      s2_value1 = _mm256_set1_pd(value);
+      s2_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
-    std::tie(s0_value0, s0_value1) = abs_neg<0>(s0_value0, s0_value1, abs, neg);
-    std::tie(s1_value0, s1_value1) = abs_neg<1>(s1_value0, s1_value1, abs, neg);
-    std::tie(s2_value0, s2_value1) = abs_neg<2>(s2_value0, s2_value1, abs, neg);
+    s0_value = abs_neg<0>(s0_value, abs, neg);
+    s1_value = abs_neg<1>(s1_value, abs, neg);
+    s2_value = abs_neg<2>(s2_value, abs, neg);
 
-    auto d_value0 = _mm256_fmadd_pd(s0_value0, s1_value0, s2_value0);
-    auto d_value1 = _mm256_fmadd_pd(s0_value1, s1_value1, s2_value1);
+    const auto d_value0 = _mm256_fmadd_pd(s0_value.value0, s1_value.value0, s2_value.value0);
+    const auto d_value1 = _mm256_fmadd_pd(s0_value.value1, s1_value.value1, s2_value.value1);
 
-    std::tie(d_value0, d_value1) = omod_clamp(d_value0, d_value1, omod, clamp);
+    const auto d_value = omod_clamp(m256dx2(d_value0, d_value1), omod, clamp);
 
-    store_vgpr(elem, d, d_value0, d_value1, vgprs, mask);
+    store_vgpr(elem, d, d_value, vgprs, mask);
   }
 }
 
@@ -165,33 +180,31 @@ void v_add_f64_e64(uint16_t d, uint16_t s0, uint16_t s1, uint8_t abs, uint8_t ne
     const auto i = static_cast<int32_t>(sgprs[126] >> elem);
     const auto mask = _mm256_cmpeq_epi32(_mm256_and_si256(_mm256_set1_epi32(i), bitpos), bitpos);
 
-    __m256d s0_value0, s0_value1, s1_value0, s1_value1;
+    m256dx2 s0_value, s1_value;
 
     if (s0 >= 256) {
-      std::tie(s0_value0, s0_value1) = load_vgpr(elem, s0 - 256, vgprs);
+      s0_value = load_vgpr(elem, s0 - 256, vgprs);
     } else {
       const auto value = get_scalar_operand_e64_f64(s0, sgprs, literal_constant);
-      s0_value0 = _mm256_set1_pd(value);
-      s0_value1 = _mm256_set1_pd(value);
+      s0_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
     if (s1 >= 256) {
-      std::tie(s1_value0, s1_value1) = load_vgpr(elem, s1 - 256, vgprs);
+      s1_value = load_vgpr(elem, s1 - 256, vgprs);
     } else {
       const auto value = get_scalar_operand_e64_f64(s1, sgprs, literal_constant);
-      s1_value0 = _mm256_set1_pd(value);
-      s1_value1 = _mm256_set1_pd(value);
+      s1_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
-    std::tie(s0_value0, s0_value1) = abs_neg<0>(s0_value0, s0_value1, abs, neg);
-    std::tie(s1_value0, s1_value1) = abs_neg<1>(s1_value0, s1_value1, abs, neg);
+    s0_value = abs_neg<0>(s0_value, abs, neg);
+    s1_value = abs_neg<1>(s1_value, abs, neg);
 
-    auto d_value0 = _mm256_add_pd(s0_value0, s1_value0);
-    auto d_value1 = _mm256_add_pd(s0_value1, s1_value1);
+    const auto d_value0 = _mm256_add_pd(s0_value.value0, s1_value.value0);
+    const auto d_value1 = _mm256_add_pd(s0_value.value1, s1_value.value1);
 
-    std::tie(d_value0, d_value1) = omod_clamp(d_value0, d_value1, omod, clamp);
+    const auto d_value = omod_clamp(m256dx2(d_value0, d_value1), omod, clamp);
 
-    store_vgpr(elem, d, d_value0, d_value1, vgprs, mask);
+    store_vgpr(elem, d, d_value, vgprs, mask);
   }
 }
 
@@ -203,21 +216,22 @@ void v_mul_f64_e32(uint16_t d, uint16_t s0, uint16_t s1, const int32_t *sgprs, i
     const auto i = static_cast<int32_t>(sgprs[126] >> elem);
     const auto mask = _mm256_cmpeq_epi32(_mm256_and_si256(_mm256_set1_epi32(i), bitpos), bitpos);
 
-    __m256d s0_value0, s0_value1, s1_value0, s1_value1;
+    m256dx2 s0_value;
 
     if (s0 >= 256) {
-      std::tie(s0_value0, s0_value1) = load_vgpr(elem, s0 - 256, vgprs);
+      s0_value = load_vgpr(elem, s0 - 256, vgprs);
     } else {
       const auto value = get_scalar_operand_e64_f64(s0, sgprs, literal_constant);
-      s0_value0 = _mm256_set1_pd(value);
-      s0_value1 = _mm256_set1_pd(value);
+      s0_value = m256dx2(_mm256_set1_pd(value), _mm256_set1_pd(value));
     }
 
-    std::tie(s1_value0, s1_value1) = load_vgpr(elem, s1, vgprs);
+    const auto s1_value = load_vgpr(elem, s1, vgprs);
 
-    auto d_value0 = _mm256_mul_pd(s0_value0, s1_value0);
-    auto d_value1 = _mm256_mul_pd(s0_value1, s1_value1);
+    const auto d_value0 = _mm256_mul_pd(s0_value.value0, s1_value.value0);
+    const auto d_value1 = _mm256_mul_pd(s0_value.value1, s1_value.value1);
 
-    store_vgpr(elem, d, d_value0, d_value1, vgprs, mask);
+    const auto d_value = m256dx2(d_value0, d_value1);
+
+    store_vgpr(elem, d, d_value, vgprs, mask);
   }
 }
